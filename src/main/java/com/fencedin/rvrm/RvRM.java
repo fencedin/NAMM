d79d1d4383f9a74828e3d0e0c1dcff2b149f3ce8
Vincent Rotter <vincent.rotter@gmail.com>
2014-10-24 13:51:24 -0700

    add base mod class

---
+++ src/main/java/com/fencedin/rvrm/RvRM.java
@@ -0,0 +1,31 @@
+package com.fencedin.rvrm;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "RvRM", name = "Risk vs Reward Mod", version = "1.7.10-0.1")
+
+public class RvRM
+{
+    @Mod.Instance("RvRM")
+    public static RvRM instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        //load network handling, mod config, items, blocks
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent)
+    {
+        //register GUI, recipies, tile entites, genral handlers
+    }
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent)
+    {
+        //other, stuff after other mods
+    }
+}
